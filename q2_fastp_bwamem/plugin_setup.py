a207179eeb25a8f6065b3ea9e2516c225de7f86b
Grant Cheng <chengxgrant@gmail.com>
2022-07-27 14:38:29 -0700

    changed version

--- q2_fastp_bwamem/plugin_setup.py
+++ q2_fastp_bwamem/plugin_setup.py
@@ -1,9 +1,8 @@
 from qiime2.plugin import Plugin
-import q2_fastp_bwamem
 
 plugin = Plugin(
     name='fastp-bwamem',
-    version=q2_fastp_bwamem.__version__,
+    version="v0.0.1",
     website='https://github.com/CatFish47/q2-fastp-bwamem',
     user_support_text='https://github.com/CatFish47/q2-fastp-bwamem',
     package='q2_fastp_bwamem'
